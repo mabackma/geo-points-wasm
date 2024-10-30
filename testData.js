ffa2274b02576f24555bb43f8aea84eefcf2f7b1
mabackma <marc.backman@edu.lapinamk.fi>
2024-10-30 05:32:17 +0200

    testing simulation

---
+++ testData.js
@@ -0,0 +1,110 @@
+export const areaPolygonsExample = JSON.stringify(
+    {
+        "pointProperty": {
+                "Point": {
+                    "coordinates": "428124.1904,7370628.218"
+                }
+            },
+        "polygonProperty": {
+            "Polygon": {
+                "exterior": {
+                    "LinearRing": {
+                        "coordinates": "428174.9494,7370648.3942 428178.9512,7370650.8309 428181.5383,7370655.6023 428181.3213,7370661.9528 428178.0232,7370668.463 428172.9192,7370676.351 428171.5272,7370682.383 428172.4552,7370689.8071 428175.3582,7370695.7077 428180.5292,7370710.2441 428189.8579,7370728.0942 428195.4877,7370728.2549 428216.4877,7370734.7549 428224.8078,7370734.0758 428221.4857,7370718.867 428217.9274,7370707.2071 428215.6073,7370688.647 428216.9993,7370657.5589 428217.4633,7370654.3109 428224.1674,7370629.5877 428208.1308,7370629.3281 428192.8996,7370624.7738 428179.72,7370618.9927 428136.9705,7370600.8159 428109.0866,7370593.341 428092.6469,7370617.4228 428082.9029,7370634.5909 428085.3158,7370641.528 428086.3295,7370644.997 428090.327,7370655.935 428122.3431,7370658.255 428146.2474,7370653.0241 428150.7104,7370651.9942 428162.4697,7370649.4742 428168.1004,7370646.4572 428174.9494,7370648.3942"
+                    }
+                }
+            }
+        }
+    }
+); // Example polygons in PolygonGeometry format
+
+export const treeStrataExample = JSON.stringify(
+    {
+        "TreeStratum": [
+            {
+                "ChangeState": 0,
+                "StratumNumber": 0,
+                "TreeSpecies": 1,
+                "Storey": 1,
+                "Age": 54,
+                "BasalArea": 5,
+                "StemCount": 50,
+                "MeanDiameter": 14.6,
+                "MeanHeight": 10.0,
+                "Volume": 34.2,
+                "SawLogPercent": 4.7,
+                "SawLogVolume": 1.6,
+                "PulpWoodVolume": 31.3,
+                "VolumeGrowth": 0.5,
+                "DataSource": 0
+            },
+            {
+                "ChangeState": 0,
+                "StratumNumber": 0,
+                "TreeSpecies": 1,
+                "Storey": 1,
+                "Age": 48,
+                "BasalArea": 3.3,
+                "StemCount": 50,
+                "MeanDiameter": 15,
+                "MeanHeight": 10.2,
+                "Volume": 20.2,
+                "SawLogPercent": 0.5,
+                "SawLogVolume": 0.1,
+                "PulpWoodVolume": 19.4,
+                "VolumeGrowth": 0.5,
+                "DataSource": 0
+            },
+            {
+                "ChangeState": 0,
+                "StratumNumber": 0,
+                "TreeSpecies": 3,
+                "Storey": 2,
+                "Age": 20,
+                "BasalArea": 1.7,
+                "StemCount": 50,
+                "MeanDiameter": 4,
+                "MeanHeight": 3.4,
+                "Volume": 5.3,
+                "SawLogPercent": 0,
+                "SawLogVolume": 0,
+                "PulpWoodVolume": 0,
+                "VolumeGrowth": 0.3,
+                "DataSource": 0
+            },
+            {
+                "ChangeState": 0,
+                "StratumNumber": 0,
+                "TreeSpecies": 3,
+                "Storey": 2,
+                "Age": 20,
+                "BasalArea": 0.6,
+                "StemCount": 50,
+                "MeanDiameter": 3.7,
+                "MeanHeight": 3,
+                "Volume": 1.6,
+                "SawLogPercent": 0,
+                "SawLogVolume": 0,
+                "PulpWoodVolume": 0.1,
+                "VolumeGrowth": 0.1,
+                "DataSource": 0
+            },
+            {
+                "ChangeState": 0,
+                "StratumNumber": 0,
+                "TreeSpecies": 3,
+                "Storey": 1,
+                "Age": 55,
+                "BasalArea": 2,
+                "StemCount": 50,
+                "MeanDiameter": 14.8,
+                "MeanHeight": 2.8,
+                "Volume": 12.7,
+                "SawLogPercent": 5.5,
+                "SawLogVolume": 0.7,
+                "PulpWoodVolume": 11.4,
+                "VolumeGrowth": 0.2,
+                "DataSource": 0
+            }
+        ]
+    }
+); // Example tree strata in TreeStrata format
